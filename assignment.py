f67800045df5eaa7a22c394f49f0ba0e6b6d50f9
shahd nalwy <s.b.nalwy@students.ptuk.edu.ps>
2025-05-17 02:17:39 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-original_image = cv2.imread(r"C:\Users\AC\Pictures\penguin.jpg") 
+original_image = cv2.imread(r"C:\Users\AC\Pictures\penguin.jpg")  
 
 #1. Read an Image
 cv2.imshow("Original Image", original_image)
@@ -10,17 +10,17 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 #2. Change Color of Top-Right Quarter to light pink
-image = original_image.copy()
-h, w, _ = image.shape
-image[:h//2, w//2:] = [255, 200, 255]  
-cv2.imshow("Top-Right Colored (light pink)", image)
+i = original_image.copy()
+h, w, _ = i.shape
+i[:h//2, w//2:] = [255, 200, 255]  
+cv2.imshow("Top-Right Colored (light pink)", i)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # 3. Brighten Bottom-Left Quarter
-image = original_image.copy()
-image[h//2:, :w//2] = cv2.add(image[h//2:, :w//2], 80)  # Increase brightness by 80
-cv2.imshow("Bottom-Left Brightened", image)
+sh = original_image.copy()
+sh[h//2:, :w//2] = cv2.add(sh[h//2:, :w//2], 80)  # Increase brightness by 80
+cv2.imshow("Bottom-Left Brightened", sh)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
@@ -33,15 +33,15 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # 5. Remove noise using Gaussian Blur
-image = original_image.copy()
-blurred = cv2.GaussianBlur(image, (5, 5), 0)
+mm= original_image.copy()
+blurred = cv2.GaussianBlur(mm, (5, 5), 0)
 cv2.imshow("Noise Removed", blurred)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # 6. Apply insulation using a threshold.
-image = original_image.copy()
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ss = original_image.copy()
+gray = cv2.cvtColor(ss, cv2.COLOR_BGR2GRAY)
 _, thresh = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
 cv2.imshow("Thresholding", thresh)
 cv2.waitKey(0)
@@ -51,9 +51,8 @@ cv2.destroyAllWindows()
 image = original_image.copy()
 watermark = cv2.imread("C:\\Users\\AC\\Pictures\\SH.png", cv2.IMREAD_UNCHANGED)
 
-watermark.shape[2] == 4
 watermark = cv2.cvtColor(watermark, cv2.COLOR_BGRA2BGR)
-
+   
 width_cm, height_cm = 4,4 
 dpi = 200 
 new_w, new_h = int(width_cm * dpi / 2.54), int(height_cm * dpi / 2.54)
@@ -79,11 +78,16 @@ hsv = cv2.cvtColor(image[:h_part, :], cv2.COLOR_BGR2HSV)  # Upper part → HSV
 gray = cv2.cvtColor(image[h_part:2*h_part, :], cv2.COLOR_BGR2GRAY)  # Middle part → Grayscale
 lab = cv2.cvtColor(image[2*h_part:, :], cv2.COLOR_BGR2LAB)  # Bottom → LAB
 
-
 gray_colored = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)  # Convert Grayscale to BGR for proper merging
 merged_image = np.vstack((hsv, gray_colored, lab))  
 
 cv2.imshow("Color Space Conversion (HSV, Grayscale, LAB)", merged_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
+
+
+
 
